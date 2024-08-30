e3bdac7259f0fc8859ce72ddc9e9213727af4abe
YA <9039417+yzkn@users.noreply.github.com>
2024-08-31 08:35:26 +0900

    Update split_without_header.ps1

--- Util/split_without_header.ps1
+++ Util/split_without_header.ps1
@@ -1,1 +1,4 @@
+$SJIS = [Text.Encoding]::GetEncoding("Shift-JIS")
+#  -Encoding UTF8   or   -Encoding $SJIS
+
 $i = 0; Get-Content original.csv -Encoding UTF8 -ReadCount 3000000 | % { $_ | Out-File "original_$i.csv" -Encoding UTF8 -Append; $i++ }
